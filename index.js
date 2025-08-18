6a843b369022826b4bbe7638e61a6227ea752e65
DonnaDoughnut <amandaxu.hualong@gmail.com>
2025-08-17 21:34:35 -0400

    quizzes

--- index.js
+++ index.js
@@ -29,8 +29,8 @@ const sessionOptions = {
 if (process.env.NODE_ENV !== "development") {
     sessionOptions.proxy = true;
     sessionOptions.cookie = {
-        sameSite: "lax",
-        secure: false,
+        sameSite: "none",
+        secure: true,
         domain: process.env.NODE_SERVER_DOMAIN,
     };}
       
